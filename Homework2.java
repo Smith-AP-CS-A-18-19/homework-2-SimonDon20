72bdf61f90c2f94410247c1a08cc4b76da3db098
Donovan <SimonDon@berkeleyprep.org>
2018-09-17 20:17:27 -0400

    Completed!

--- Homework2.java
+++ Homework2.java
@@ -1,5 +1,5 @@
 /*
- * First and Last Names
+ * Donovan Simonton
  */
 
 import java.awt.Rectangle;
@@ -10,16 +10,22 @@ public class Homework2 {
 	 * It takes in one parameter: a String
 	 * Save the string as a private instance variable
 	 */
+	 private String input_String;
+	 	public Homework2(String input) {
+			input_String = input;
+		}
 
-
-	/* Write the method upperase
+	/* Write the method uppercase
 	 * It does not take in any parameters
 	 * It returns a String
 	 * Use String methods to turn the instance variable String into all
 	 * uppercase letters and return the result
 	 * Hint: Search the String class for 'uppercase'
 	 */
-
+	 public String uppercase(){
+		 input_String = input_String.toUpperCase();
+		 return input_String;
+	 }
 
 	/* Write the method strip
 	 * It takes in one paramter: a String
@@ -28,17 +34,23 @@ public class Homework2 {
 	 * It should remove spaces, commas, periods, and exclamation points.
 	 * Hint: Search the String class for 'replace'
 	 */
-
+	 public String strip(String input_String){
+		 input_String = input_String.replace(" ", "");
+		 input_String = input_String.replace(",", "");
+		 input_String = input_String.replace(".", "");
+		 input_String = input_String.replace("!", "");
+		 return input_String;
+	 }
 
 	/* Fix the errors in the method problem4
 	 */
 	public Rectangle problems() {
-		Rectangle r1 = (5, 10, 15, 20);
-		double width = Rectangle(5, 10, 15, 20).getWidth();
-		Rectangle r2;
+		Rectangle r1 = new Rectangle(5, 10, 15, 20);
+		double width = r1.getWidth();
+		Rectangle r2 = new Rectangle(0, 0, 1, 1);
 		r2.translate(15, 25);
-		r3 = new Rectangle();
-		r3.translate("Far, far away");
+		Rectangle r3 = new Rectangle(0, 0, 1, 1);
+		r3.translate(3000, 5000);
 		return r3;
 	}
 
@@ -47,6 +59,10 @@ public class Homework2 {
 	 * parameter Rectangle
 	 */
 	public double getPerimeter(Rectangle rect) {
+		double width = rect.getWidth();
+		double height = rect.getHeight();
+		double perimeter = (2 * width) + (2 * height);
+		return perimeter;
 
 	}
 
